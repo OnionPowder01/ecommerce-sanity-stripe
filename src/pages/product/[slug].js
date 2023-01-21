b2f3fb5eb6ea32b9bfde398033df3dbefead90b2
OnionPowder01 <autonatione36@gmail.com>
2023-01-21 16:02:40 +0200

    stateContext

--- src/pages/product/[slug].js
+++ src/pages/product/[slug].js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-
 import {
   AiOutlineMinus,
   AiOutlinePlus,
@@ -7,30 +6,36 @@ import {
   AiOutlineStar,
 } from "react-icons/ai";
 import { client, urlFor } from "lib/client";
-
 import { Product } from "components";
+import { useStateContext } from "context/StateContext";
 
 const ProductDetails = ({ products, product }) => {
   const { image, name, details, price } = product;
   const [index, setIndex] = useState(0);
-
+  const { decQty, incQty, qty, onAdd } = useStateContext();
   return (
     <div>
       <div className="product-detail-container">
         <div>
           <div className="image-container">
-            <img src={urlFor(image && image[index])} className='product-detail-image' alt="" />
+            <img
+              src={urlFor(image && image[index])}
+              className="product-detail-image"
+              alt=""
+            />
           </div>
-          <div className='small-images-container'>
-                {image?.map((item, i) => (
-                 <img 
-                    key={i}
-                    src={urlFor(item)}
-                    className={i === index ? 'small-image selected-image' : 'small-image'}
-                    onMouseEnter={() => setIndex(i)}
-                    />
-                   ))}     
-                </div>
+          <div className="small-images-container">
+            {image?.map((item, i) => (
+              <img
+                key={i}
+                src={urlFor(item)}
+                className={
+                  i === index ? "small-image selected-image" : "small-image"
+                }
+                onMouseEnter={() => setIndex(i)}
+              />
+            ))}
+          </div>
         </div>
 
         <div className="product-detail-desc">
@@ -49,19 +54,19 @@ const ProductDetails = ({ products, product }) => {
           <div className="quantity">
             <h3>Quantity: </h3>
             <p className="quantity-desc">
-              <span className="minus" onClick="">
+              <span className="minus" onClick={decQty}>
                 <AiOutlineMinus />
               </span>
               <span className="num" onClick="">
-                0
+                {qty}
               </span>
-              <span className="plus" onClick="">
+              <span className="plus" onClick={incQty}>
                 <AiOutlinePlus />
               </span>
             </p>
           </div>
           <div className="buttons">
-            <button type="button" className="add-to-cart" onClick="">
+            <button type="button" className="add-to-cart" onClick={ () => onAdd(product, qty)}>
               Add to Cart
             </button>
             <button type="button" className="buy-now" onClick="">
