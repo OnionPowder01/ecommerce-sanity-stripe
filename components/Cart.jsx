f7b326c0fb52cdeed8318777d4c44405c138c4bc
OnionPowder01 <autonatione36@gmail.com>
2023-01-22 15:52:32 +0200

    stripe

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -10,13 +10,35 @@ import { TiDeleteOutline } from "react-icons/ti";
 import toast from "react-hot-toast";
 import { useStateContext } from "context/StateContext";
 import { urlFor } from "lib/client";
-import product from "sanityecommerce/schemas/product";
+import getStripe from "lib/getStripe";
 
 const Cart = () => {
   const cartRef = useRef();
   const { totalPrice, totalQuantities, cartItems, setShowCart , toggleCartItemQuantity, onRemove } =
     useStateContext();
-console.log(cartItems)
+
+    const handleCheckout = async () => {
+      const stripe = await getStripe();
+  
+      const response = await fetch('/api/stripe', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(cartItems),
+      });
+  
+      if(response.statusCode === 500) return;
+
+
+      
+      const data = await response.json();
+  
+      toast.loading('Redirecting...');
+  
+      stripe.redirectToCheckout({ sessionId: data.id });
+    }
+
   return (
     <div className="cart-wrapper" ref={cartRef}>
       <div className="cart-container">
@@ -87,7 +109,7 @@ console.log(cartItems)
                   <h3>${totalPrice.toFixed(2)}</h3>
               </div>
               <div className="btn-container">
-                <button type='button' className="btn" onClick=''>
+                <button type='button' className="btn" onClick={handleCheckout}>
                   Pay with Stripe
                 </button>
               </div>
